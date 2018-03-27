b21d3acf7d7c0f3c0032c0bb151b7339c483bf53
Lukas Haak <lukas.haak33@gmail.com>
2018-03-27 16:21:03 +1100

    Dice calc added, also dice script is more user friendly

---
+++ Assets/Lukas/Scripts/diceBase.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diceBase : MonoBehaviour {
+
+
+    public int selectedVector;
+
+    public int[] dieNumber;
+    public Vector3[] vectorPoints;  //Number of sides, as it's one vector per side.
+
+	// Use this for initialization
+	void Start ()
+    {
+        //gameObject
+
+        int dieSize = vectorPoints.Length;
+
+        dieNumber = new int[dieSize];
+
+        dieNumber[0] = vectorPoints.Length;
+
+		for(int i = 1; i < vectorPoints.Length; i++)
+        {
+            dieNumber[i] = i;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float currentBest = -1;
+
+        for(int i = 0; i < vectorPoints.Length; ++i)
+        {
+            var vectorValue = vectorPoints[i];
+            var worldSpaceValueVector = this.transform.localToWorldMatrix.MultiplyVector(vectorValue);
+
+            float dot = Vector3.Dot(worldSpaceValueVector, Vector3.up);
+
+            if (dot > currentBest)
+            {
+                currentBest = dot;
+                selectedVector = dieNumber[i];
+            }
+        }
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        foreach (var valueVector in vectorPoints)
+        {
+            var worldSpaceValueVector = this.transform.localToWorldMatrix.MultiplyVector(valueVector);
+
+            Gizmos.DrawLine(this.transform.position, this.transform.position + worldSpaceValueVector);
+        }
+    }
+}
---
+++ Assets/Lukas/Scripts/diceCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diceCalculator : MonoBehaviour {
+
+    public GameObject[] diceInPlay;
+    public int totalRolled;
+
+	// Use this for initialization
+	void Start ()
+    {
+       //diceInPlay = GameObject.FindGameObjectsWithTag("dice");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetTotalRolled();
+        }
+	}
+
+    void GetTotalRolled ()
+    {
+        totalRolled = 0;
+        diceInPlay = GameObject.FindGameObjectsWithTag("dice");
+
+        foreach (var myDice in diceInPlay)
+        {
+            totalRolled += myDice.GetComponent<diceBase>().selectedVector;
+        }
+    }
+}
