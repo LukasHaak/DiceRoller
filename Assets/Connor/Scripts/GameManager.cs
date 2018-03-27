91476b30f968cdb403aa1543dff114e8758bc16a
Condogs <Connor.howells@hotmail.com>
2018-03-27 16:22:48 +1100

    Dice Roller Update

---
+++ Assets/Connor/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public int DiceResult;
+
+	// Use this for initialization
+	void Start () {
+		DiceResult = 0;
+	}
+	
+	
+	void DisplayResult()
+	{
+		Debug.Log("RESULT: " + DiceResult);
+	}
+
+}
